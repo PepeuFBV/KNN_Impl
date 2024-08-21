e6b9f258e48f056d3d797acb6bb605ae4bbb9ae3
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-08-21 20:40:49 -0300

    fixes + pca test

--- KNN.py
+++ KNN.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 def euclidean_distance(x1, x2):
-    return np.sqrt(np.sum((x1 - x2) ** 2))
+    return np.sqrt(np.sum((x1 - x2) ** 2)) # Calculate the Euclidean distance between two points, handles multiple
+    # dimensions as it calculates the distance between each dimension and sums them up, then takes the square root of
+    # that sum
 
 def manhattan_distance(x1, x2):
     return np.sum(np.abs(x1 - x2))
@@ -16,26 +18,21 @@ class KNN:
         self.y_train = None
         self.method = euclidean_distance
 
-    def fit(self, x, y, method=euclidean_distance):
+    def fit(self, x, y, method = euclidean_distance):
         self.x_train = x
         self.y_train = y
         self.method = method
 
     def predict(self, x):
-        predictions = [self._predict(i) for i in x] # calls secondary predict function
+        predictions = []
+        for i in x:
+            distances = [self.method(i, x_train) for x_train in self.x_train] # Calculate the distance between the
+            # test point in comparison to all the training points in the dataset
+            k_indexes_found = np.argsort(distances)[:self.k] # Get the indexes of the k nearest points
+            k_values = [self.y_train[j] for j in k_indexes_found] # Get the values of the k nearest points
+            most_common = np.bincount(k_values).argmax() # Get the most common value of the k nearest points
+            predictions.append(most_common)
         return predictions
-
-    # Brute force method
-    def _predict(self, x):
-        distances = [self.method(x, x_train) for x_train in self.x_train]
-
-        # get the nearest neighbours, argsort returns the indexes that would sort the array without doing it
-        k_indexes_found = np.argsort(distances)[:self.k] # gets only the k closest
-        k_values = [self.y_train[i] for i in k_indexes_found]
-
-        # return the most common class label
-        most_commmon = np.bincount(k_values).argmax()
-        return most_commmon
 
     def get_params(self, deep=True):
         return {"k": self.k}
--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from sklearn.metrics import classification_report
+import seaborn as sns
 from KNN import KNN
 cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
-# Visualizing the data
+
+# Visualizing the dataset
+
 diabetes = pd.read_csv('diabetes.csv')
 # print all columns in 1 line
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
+print("The first 5 rows of the dataset are:")
 print(diabetes.head(5))
 diabetes.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+plt.suptitle("Dataset columns")
 plt.show()
 
 X, y = diabetes.iloc[:, :-1].values, diabetes.iloc[:, -1].values
@@ -23,38 +29,40 @@ predictions = model.predict(X_test)
 
 accuracy = np.sum(predictions == y_test) / len(y_test) # number of correct predictions divided by total number of predictions
 
-print(f"{accuracy * 100:.2f}% is the accuracy of the model without preprocessing the data")
-from sklearn.metrics import classification_report
-print(classification_report(y, predictions))
+print("\n\nAfter training the model with the dataset without preprocessing the data, the results are:")
+print(f"{accuracy * 100:.2f}% is the accuracy of the model without preprocessing the data\n")
+print(classification_report(y_test, predictions))
+print("\n")
 
-
-# Confusion matrix for dataset without preprocessing
-import seaborn as sns
+# Correlation matrix for the dataset without preprocessing
 x_clean_df = pd.DataFrame(X, columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI',
                                       'DiabetesPedigreeFunction', 'Age'])
 x_clean_df['Outcome'] = y
 corr = x_clean_df.corr()
 sns.heatmap(corr, annot=True)
+plt.suptitle("Correlation matrix for the dataset without preprocessing")
 plt.show()
 
 
 #################################################
 
 
-# Removing columns with low correlation to the outcome
+# Removing columns with low correlation to the outcome, based on the correlation matrix
 # Convert x_clean to a DataFrame
 x_df = pd.DataFrame(X, columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI',
                                       'DiabetesPedigreeFunction', 'Age'])
 
 # Select only the desired columns in the non-outcome columns, based on the correlation matrix results
-x_clean_df = x_clean_df[['Pregnancies', 'Glucose', 'BMI', 'Age']]
+x_clean_df = x_clean_df[['Pregnancies', 'Glucose', 'BMI', 'Age', 'Insulin', 'DiabetesPedigreeFunction']]
 
 # Plot the selected columns
-x_clean_df.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+x_clean_df.plot(kind='box', subplots=True, layout=(3, 3), sharex=False, sharey=False)
+plt.suptitle("Selected columns for model")
 plt.show()
 
 
 #################################################
+
 
 # Removing outliers from all columns except the last one
 indices_to_keep = np.ones(len(x_clean_df), dtype=bool)
@@ -71,8 +79,10 @@ x_clean_df = x_clean_df[indices_to_keep].reset_index(drop=True)
 y = y[indices_to_keep]
 
 # Plot the cleaned data
-x_clean_df.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+x_clean_df.plot(kind='box', subplots=True, layout=(3, 3), sharex=False, sharey=False)
+plt.suptitle("Cleaned data, without outliers")
 plt.show()
+
 
 #################################################
 
@@ -84,13 +94,9 @@ x_clean = scaler.fit_transform(x_clean_df)
 
 # Use the original column names for the normalized DataFrame
 x_clean_plot = pd.DataFrame(x_clean, columns=x_clean_df.columns)
-x_clean_plot.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+x_clean_plot.plot(kind='box', subplots=True, layout=(3, 3), sharex=False, sharey=False)
+plt.suptitle("Normalized data")
 plt.show()
-
-# After using PCA to reduce the number of features, the results were worse
-# from sklearn.decomposition import PCA
-# pca = PCA(n_components=3)
-# x_clean = pca.fit_transform(x_clean)
 
 
 #################################################
@@ -101,9 +107,8 @@ X_train, X_test, y_train, y_test = train_test_split(x_clean, y, test_size=0.25)
 knn.fit(X_train, y_train)
 predictions = knn.predict(X_test)
 knn_accuracy = np.sum(predictions == y_test) / len(y_test)
-print(f"{knn_accuracy * 100:.2f}% is the accuracy of the model after preprocessing the data")
-from sklearn.metrics import classification_report
-print(classification_report(y, predictions))
+print(f"{knn_accuracy * 100:.2f}% is the accuracy of the model after preprocessing the data\n")
+print(classification_report(y_test, predictions))
 
 
 #################################################
@@ -114,30 +119,31 @@ from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import accuracy_score
 k = list(range(1, 100))
 scores = []
-print("\nCalculating k values...")
+print("\nCalculating for [1, 100] k values...")
 for i in k:
     knn = KNN(i)
     predictions = cross_val_predict(knn, x_clean, y, cv=10)
     scores.append(accuracy_score(y, predictions))
 
 plt.plot(k, scores)
+plt.suptitle("Accuracy for different k values")
 plt.xlabel('k')
 plt.ylabel('Accuracy')
 plt.show()
 
 best_k = k[scores.index(max(scores))]
-print(f"The best k value is {best_k} for a 10-fold cross validation")
+print(f"The best k value is {best_k} for a 10-fold cross validation with a accuracy of {max(scores)*100}%")
 
 
 #################################################
 
 
-k = list(range(15, 70))
-cv = list(range(4, 15))
+k = list(range(9, 55))
+cv = list(range(3, 15))
 best_scores = []
 cv_for_best_scores = []
 best_result = [0, 0]
-print("\nCalculating best cv value for each k value...")
+print("\nCalculating best cv [3, 15] value for each k [9, 55] value...")
 for i in k:
     scores = []
     for j in cv:
@@ -148,21 +154,49 @@ for i in k:
     cv_for_best_scores.append(cv[scores.index(max(scores))])
     if max(scores) > best_result[0]:
         best_result = [max(scores), i]
-    print(f"Best cv value for k = {i} is {cv[scores.index(max(scores))]}")
-print(f"\nThe best k value is {best_result[1]} with a cv value of {cv_for_best_scores[k.index(best_result[1])]}")
+print(f"\nThe best k value is {best_result[1]} with a cv value of {cv_for_best_scores[k.index(best_result[1])]}, "
+      f"with an accuracy of {best_result[0]*100}%")
 
 plt.plot(k, best_scores)
+plt.suptitle("Accuracy for different k values with best cv value")
 plt.xlabel('k')
 plt.ylabel('Accuracy')
 plt.show()
 
+
 #################################################
 
-# Using the best k value and cv value
+
+# Using PCA to reduce the number of features and test results
+from sklearn.decomposition import PCA
+pca_range = list(range(1, 4))
+k_value = 12
+scores = []
+print("\nCalculating for [1, 3] PCA components...")
+for i in pca_range:
+    pca = PCA(n_components=i)
+    x_pca = pca.fit_transform(x_clean)
+    knn = KNN(k_value)
+    predictions = cross_val_predict(knn, x_pca, y, cv=10)
+    scores.append(accuracy_score(y, predictions))
+
+plt.plot(pca_range, scores)
+plt.suptitle("Accuracy for different PCA components, with k = 12 and no cross validation")
+plt.xlabel('PCA components')
+plt.ylabel('Accuracy')
+plt.show()
+
+# Since the accuracy with the PCA components is lower than the accuracy without PCA, we will not use PCA for the
+# final test, only the best k and cv values found
+
+
+#################################################
+
+# Using the best k value and cv value for last test without PCA
 
 knn = KNN(best_result[1])
 predictions = cross_val_predict(knn, x_clean, y, cv=cv_for_best_scores[k.index(best_result[1])])
 accuracy = accuracy_score(y, predictions)
-
-from sklearn.metrics import classification_report
+print(f"{accuracy * 100:.2f}% is the accuracy of the model after preprocessing the data, with the usage of the best "
+      f"k, best cv and no PCA usage")
 print(classification_report(y, predictions))
